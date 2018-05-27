f3a7b092dc5dfaed3db696a5285036b4934c39cc
div <divij@hello.com>
2018-05-26 20:16:00 -0700

    initial Commit

---
+++ app/src/main/java/div_bytes/com/practicearch/MainActivity.kt
@@ -0,0 +1,19 @@
+package div_bytes.com.practicearch
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import div_bytes.com.practicearch.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/div_bytes/com/practicearch/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package div_bytes.com.practicearch.ui.main
+
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
